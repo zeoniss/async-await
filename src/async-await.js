171ae8d20e835848fcdc367379af82bbeace9315
Vladimir Kriukov <zeoniss28@gmail.com>
2021-02-25 21:45:37 +0200

    summary

--- src/async-await.js
+++ src/async-await.js
@@ -2,14 +2,14 @@
  * Асинхронные функции возвращают промис
  */
 
-// const getFruit = async (name) => {
-//   const fruits = {
-//     apple: "🍎",
-//     peach: "🍑",
-//     grapes: "🍇",
-//   };
+// const getFruit = async(name) => {
+//     const fruits = {
+//         apple: "🍎",
+//         peach: "🍑",
+//         grapes: "🍇",
+//     };
 
-//   return fruits[name];
+//     return fruits[name];
 // };
 
 // getFruit("apple").then((fruit) => console.log(fruit));
@@ -17,13 +17,13 @@
  * Избегаем promise hell и чейнинг с ошибкой паралельного выполнения
  */
 
-const makePromiseSmothie = () => {
-  return getFruit("apple").then((apple) => {
-    return getFruit("peach").then((peach) => {
-      return [apple, peach];
-    });
-  });
-};
+// const makePromiseSmothie = () => {
+//     return getFruit("apple").then((apple) => {
+//         return getFruit("peach").then((peach) => {
+//             return [apple, peach];
+//         });
+//     });
+// };
 
 // makePromiseSmothie().then((smothie) => console.log(smothie));
 
@@ -40,34 +40,34 @@ const makePromiseSmothie = () => {
  * Исправляем ошибку паралельного исполнения
  */
 
-const delay = (ms) => {
-  return new Promise((resolve) => setTimeout(() => resolve(""), ms));
-};
+// const delay = (ms) => {
+//     return new Promise((resolve) => setTimeout(() => resolve(""), ms));
+// };
 
-const getFruit = async (name) => {
-  const fruits = {
-    apple: "🍎",
-    peach: "🍑",
-    grapes: "🍇",
-  };
+// const getFruit = async(name) => {
+//     const fruits = {
+//         apple: "🍎",
+//         peach: "🍑",
+//         grapes: "🍇",
+//     };
 
-  await delay(1000);
+//     await delay(1000);
 
-  return fruits[name];
-};
+//     return fruits[name];
+// };
 
-// const makeAsyncSmothie = async () => {
-//   console.time("время на резолв промисов");
+// const makeAsyncSmothie = async() => {
+//     console.time("время на резолв промисов");
 
-//   const apple = getFruit("apple");
-//   const peach = getFruit("peach");
-//   const grapes = getFruit("grapes");
+//     const apple = getFruit("apple");
+//     const peach = getFruit("peach");
+//     const grapes = getFruit("grapes");
 
-//   const smothie = await Promise.all([apple, peach, grapes]);
+//     const smothie = await Promise.all([apple, peach, grapes]);
 
-//   console.timeEnd("время на резолв промисов");
+//     console.timeEnd("время на резолв промисов");
 
-//   return smothie;
+//     return smothie;
 // };
 
 // makeAsyncSmothie().then((smothie) => console.log(smothie));
@@ -76,22 +76,22 @@ const getFruit = async (name) => {
  * Обработка ошибок
  */
 
-const makeAsyncSmothie = async () => {
-  try {
-    const apple = getFruit("apple");
-    const peach = getFruit("peach");
+const makeAsyncSmothie = async() => {
+    try {
+        const apple = getFruit("apple");
+        const peach = getFruit("peach");
 
-    const smothie = await Promise.all([apple, peach]);
+        const smothie = await Promise.all([apple, peach]);
 
-    throw "Поломалось 💩";
+        throw "Поломалось 💩";
 
-    return smothie;
-  } catch (error) {
-    console.log("Error " + error);
-    throw error;
-  }
+        return smothie;
+    } catch (error) {
+        console.log("Error " + error);
+        throw error;
+    }
 };
 
 makeAsyncSmothie()
-  .then((smothie) => console.log(smothie))
-  .catch((error) => console.log(error));
+    .then((smothie) => console.log(smothie))
+    .catch((error) => console.log(error));
